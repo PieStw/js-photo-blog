5cbc2d80a5739b5ad37e185cc257aaa96d3c9206
PieStw <piero.stalteri@icloud.com>
2024-11-07 15:23:59 +0100

    creazione dinamica delle card

---
+++ script.js
@@ -0,0 +1,60 @@
+
+const postList = [];
+
+
+async function getPosts() {
+    await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+    .then(response => response.json())
+    .then((data) => {
+        for(let i = 0; i < 6; i++){
+            postList[i].id = data[i].id;
+            postList[i].text = data[i].body;
+        }
+      });
+}
+
+async function getImgs() {
+    await fetch('https://jsonplaceholder.typicode.com/photos?_limit=6')
+    .then(response => response.json())
+    .then((data) => {
+        for(let i = 0; i < 6; i++){
+            postList[i].img = data[i].url;
+        }
+      });
+}
+
+async function generatePosts() {
+
+    await getPosts(member);
+    await getImgs(member);
+
+    console.log(postList[0]);
+
+    console.log(postList);
+
+    for(let i = 0; i < 6; i++){
+        
+        document.getElementById("main").innerHTML += `
+        <div class="col-12 col-md-6 col-lg-4 card-container">
+            <div class="card">
+                <img src="${postList[i].img}" class="card-img">
+                <div class="card-body">
+                    <p class="card-text">${postList[i].text}</p>
+                </div>
+                <img src="./img/pin.svg" class="pin-img">
+            </div>
+        </div>`;
+    
+    }
+
+    document.getElementById("spinner").classList.add("d-none");
+    document.getElementById("main").classList.remove("d-none");
+
+    
+}
+
+for(let i = 0; i < 6; i++){
+    postList.push(member = {id: i, text: "", img: ""});
+}
+
+generatePosts();
